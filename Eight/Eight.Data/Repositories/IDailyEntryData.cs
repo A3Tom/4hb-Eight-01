0e5a6323382920a3fb5ab4ed8bba7803262b8791
A3Tom <Tom_Esq@Yahoo.com>
2020-12-31 10:14:49 +0000

    Updated index to address you appropriately. Added some fleshy methods to the data acceess layer. Updated the wee image in the navbar

---
+++ Eight/Eight.Data/Repositories/IDailyEntryData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Eight.Data.Interfaces;
+using Eight.Domian.Models;
+
+namespace Eight.Data.Repositories
+{
+    public class IDailyEntryData
+    {
+        public ISqlDataAccess _repository { get; }
+
+        public IDailyEntryData(ISqlDataAccess repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<DayEntry>> GetAllDayEntries()
+        {
+            var sql = "Select * From dbo.DayEntry";
+
+            return _repository.LoadData<DayEntry, dynamic>(sql, new { });
+        }
+
+        public Task InsertDayEntry(DayEntry person)
+        {
+            string sql = @"Insert into dbo.DayEntry 
+                           (Description, DateLogged, DayRating, ActivitiesCompleted)
+                           values 
+                           (@Description, @DateLogged, @DayRating, @ActivitiesCompleted);";
+
+            return _repository.SaveData(sql, person);
+        }
+    }
+}
