38b831270c58c64462e0429838b57167ba2d6569
A3Tom <Tom_Esq@Yahoo.com>
2020-12-31 09:52:22 +0000

    Removed yet more build shite. Added Data access layer, added domain layer

---
+++ Eight/Eight.Domian/Models/DayEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Eight.Domian.Models
+{
+    public class DayEntry
+    {
+        public string Description { get; init; }
+        public DateTime DateLogged { get; init; }
+        public decimal DayRating { get; init; }
+        public byte ActivitiesCompleted { get; init; }
+    }
+}
